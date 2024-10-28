61e53024b0f2c07ea4cf2d57bb0ce8484ac155bf
tantran <tantran.300803@gmail.com>
2024-10-28 23:04:49 +0700

    Refactor ormconfig.ts to remove unused imports and files

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 
-import { UserSubscriber } from './src/entity-subscribers/user-subscriber';
 import { SnakeNamingStrategy } from './src/snake-naming.strategy';
 
 dotenv.config();
@@ -14,7 +13,6 @@ export const dataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   namingStrategy: new SnakeNamingStrategy(),
-  subscribers: [UserSubscriber],
   entities: [
     'src/modules/**/*.entity{.ts,.js}',
     'src/modules/**/*.view-entity{.ts,.js}',
